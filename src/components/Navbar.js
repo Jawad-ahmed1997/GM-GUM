5c7870a2f16ec5df6ec559fc8e3f14882e7e3d5e
Jawad-ahmed1997 <jawad.walbi@gmail.com>
2022-09-07 09:04:09 +0500

    first Commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import "../../node_modules/bootstrap/dist/css/bootstrap.css"
+import Logo from "../Pics/logo.png"
+
+function Navbar() {
+  return (
+    <div className=' row d-flex justify-content-center align-item-center bg-light flex-column h-100 w-100'>
+      <div className='d-flex justify-content-center align-item-center bg-success w-100'>
+        <img src={Logo}/>
+      </div>
+      <div className='row'>
+      <div className='w-25 bg-light'>
+        <h1>GM GUM Industries</h1>
+      </div>
+      <div className='w-75 bg-primary'>
+        <h1>GM GUM Industries</h1>
+      </div>
+      </div>
+        
+        
+    </div>
+  )
+}
+
+export default Navbar
