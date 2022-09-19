9dd95dc1eceb7aeb4e7dc810333eaa286a7c6dd4
Jawad-ahmed1997 <jawad.walbi@gmail.com>
2022-09-19 08:25:47 +0500

    19-9 morning

--- src/components/Config/route.js
+++ src/components/Config/route.js
@@ -3,14 +3,11 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import {Home,About,Contact,Products} from "./index"
 
 
+
 function AppRoute() {
     return (
         <div>
             <Router>
-                <Link to="/">Home</Link>
-                <Link to="./About">About</Link>
-                <Link to="./Products">Products</Link>
-                <Link to="./contact">Contact</Link>
                 <Routes>
                     <Route path="/" element={<Home/>} />
                     <Route path="/About" element={<About/>} />
--- src/components/pages/Navbar.js
+++ src/components/pages/Navbar.js
@@ -1,23 +1,33 @@
 import React from 'react'
 import "../../../node_modules/bootstrap/dist/css/bootstrap.css"
 import Logo from "../../Pics/logo.png"
+import { Link } from 'react-router-dom'
+import '../../App.css'
 
 function Navbar() {
   return (
-    <div className=' row d-flex justify-content-center align-item-center bg-light flex-column h-100 w-100'>
-      <div className='d-flex justify-content-center align-item-center bg-success w-100'>
-        <img src={Logo}/>
-      </div>
-      <div className='row'>
-      <div className='w-25 bg-light'>
+    <div className=' row d-flex justify-content-center align-item-center flex-column h-100 w-100'>
+      <div className='d-flex justify-content-center align-item-center  w-100'>
         <h1>GM GUM Industries</h1>
       </div>
-      <div className='w-75 bg-primary'>
-          
+      <div className='w-100 flex-row d-flex justify-content-center align-item-center flex-row h-100 w-100'>
+        <div className=' d-flex justify-content-center align-item-center  w-25 bg-primary'>
+          <img className='NavImgLogo' src={Logo} />
+        </div>
+        <div className='d-flex justify-content-around align-item-center w-75 bg-dark '>
+
+          <Link className='navlink' to="/">Home</Link>
+
+          <Link className='navlink' to="./About">About</Link>
+
+          <Link className='navlink' to="./Products">Products</Link>
+
+          <Link className='navlink' to="./contact">Contact</Link>
+
+        </div>
       </div>
-      </div>
-        
-        
+
+
     </div>
   )
 }
