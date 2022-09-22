7bd6f64b613cdd01f24c463fd39489cfd349ee7e
jawad-ahmed1997 <Jawad.walbi@gmail.com>
2022-09-22 17:36:33 +0500

    COntact Update

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import Navbar from "./Navbar"
-import Slideshow from './Slider'
+import Slider from './Slider'
 
 
 function Home() {
   return (
-<<<<<<< HEAD
-    <Slider></Slider>
-=======
-   <Slideshow></Slideshow>
->>>>>>> 9f78be8c604360b9722fdd68a285f15f1bbeb99f
+    <Slider>
+
+    </Slider>
   )
 }
 
--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import BackgroundPic from '../../Pics/ContactBackground.jpg'
 
 function Contact() {
   return (
-  <div className='ContactDivMain'> 
+  <div className='ContactDivMain' style={{ backgroundImage: `url(${BackgroundPic})` }}> 
     <div className='ContactForm'>
         <form className='form'>
-          <input type={text}/>
-          <input type={text}/>
+          <input placeholder='Enter YOur Name' type="text"/>
+          <input placeholder='Enter YOur Name' type="text"/>
           <textarea>
-            
+
           </textarea>
         </form> 
     </div>
